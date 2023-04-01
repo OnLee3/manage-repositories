1eeb5e2b0f54276bdf64c229d23cc7eb6f3ced74
OnLee3 <dlwotjr2@gmail.com>
2023-04-01 21:27:05 +0900

    init

---
+++ manage_repositories.sh
@@ -0,0 +1,72 @@
+function get_repositories() {
+    gh repo list --limit 100 --json nameWithOwner | jq -r '.[] | .nameWithOwner'
+}
+
+REPOSITORIES=($(get_repositories))
+
+function select_repositories() {
+    local selected_repositories=()
+    local index=1
+
+    echo "Select repositories to manage (Enter the numbers, separated by spaces):"
+    for repo in "${REPOSITORIES[@]}"; do
+        echo "[$index] $repo"
+        index=$((index + 1))
+    done
+
+    read -ra selected_indexes
+    for index in "${selected_indexes[@]}"; do
+        selected_repositories+=("${REPOSITORIES[$((index - 1))]}")
+    done
+
+    echo "Selected repositories:"
+    printf "%s\n" "${selected_repositories[@]}"
+    echo
+
+    REPOSITORIES=("${selected_repositories[@]}")
+}
+
+select_repositories
+
+function delete_repositories() {
+    for repo in "${REPOSITORIES[@]}"; do
+        echo "Deleting repository: $repo"
+        gh repo delete "$repo" --yes
+        echo "Deleted repository: $repo"
+        echo
+    done
+}
+
+function change_visibility() {
+    local visibility="$1"
+    for repo in "${REPOSITORIES[@]}"; do
+        echo "Changing visibility of repository: $repo"
+        gh repo edit "$repo" --visibility "$visibility"
+        echo "Updated visibility of repository: $repo"
+        echo
+    done
+}
+
+echo "Choose an action:"
+echo "1. Delete repositories"
+echo "2. Make repositories public"
+echo "3. Make repositories private"
+read -p "Enter the number of your choice (1, 2, or 3): " choice
+
+case $choice in
+    1)
+        delete_repositories
+        ;;
+    2)
+        change_visibility "--public"
+        ;;
+    3)
+        change_visibility "--private"
+        ;;
+    *)
+        echo "Invalid choice. Exiting."
+        exit 1
+        ;;
+esac
+
+echo "Finished managing repositories."
